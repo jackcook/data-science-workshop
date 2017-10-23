4458a09354106320a0387d1d7cf396834ea17f29
Jack Cook <hello@jackcook.nyc>
2017-10-23 00:55:54 -0400

    Created custom theme for the graph

--- main.r
+++ main.r
@@ -7,12 +7,30 @@ library(tidyr)
 
 train <- as.tibble(fread('./Desktop/train.csv'))
 
-train %>%
+train_data <- train %>%
     mutate(hpick = hour(pickup_datetime)) %>%
     group_by(hpick, vendor_id) %>%
-    summarise(median_duration = median(trip_duration)/60) %>%
+    summarise(median_duration = median(trip_duration)/60)
+
+train_data %>%
     ggplot(aes(hpick, median_duration, color = vendor_id)) +
     geom_smooth(method = "loess", span = 1/2) +
-    geom_point(size = 4) +
-    labs(x = "Hour of the day", y = "Median trip duration [min]") +
-    theme(legend.position = "none")
+    geom_point(size = 3) +
+    labs(x = "Time", y = "Median Duration", title = "Duration of NYC Taxi Rides") +
+    scale_x_continuous(breaks = c(0, 5, 10, 15, 20), labels = c("12am", "5am", "10am", "3pm", "8pm")) +
+    scale_y_continuous(breaks = c(8, 9, 10, 11, 12), labels = c("8", "9", "10", "11", "12 minutes")) +
+    theme(
+        plot.background = element_rect(fill = "#eeeeee"),
+        panel.background = element_rect(fill = "#eeeeee"),
+        panel.grid.major = element_line(size = 0.4, linetype = "solid", colour = "#dcdcdc"),
+        panel.grid.minor = element_line(size = 0),
+        text = element_text(size = 16, family = "San Francisco Display", face = "bold"),
+        axis.title.x = element_text(margin = margin(t = 20)),
+        axis.title.y = element_text(margin = margin(r = 20)),
+        axis.text.y = element_text(hjust = 0, margin = margin(r = -50)),
+        plot.title = element_text(margin = margin(b = 20), hjust = 0.5),
+        legend.position = "none",
+        plot.margin = unit(c(1, 1.5, 1, 1), "cm"),
+        axis.ticks = element_blank()
+    )
+
